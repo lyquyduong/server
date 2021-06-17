66823ed7b762ded2a71297fce503a72a4b1fb46b
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2021-06-17 13:54:06 -0400

    Let's try this again

--- bitwarden_license/src/Portal/Utilities/EnterprisePortalTokenSignInManager.cs
+++ bitwarden_license/src/Portal/Utilities/EnterprisePortalTokenSignInManager.cs
@@ -67,19 +67,46 @@ namespace Bit.Portal.Utilities
             {
                 return error;
             }
+
+            var userId = await UserManager.GetUserIdAsync(user);
             
             try
             {
-                Logger.LogInformation("CheckTokenSignInAsync: token='{token}', user='{userId}'",
-                    token, await UserManager.GetUserIdAsync(user));
+                Logger.LogInformation("CheckTokenSignInAsync(1): user='{userId}', token='{token}'", userId, token);
                 var unprotectedData = _dataProtector.Unprotect(Convert.FromBase64String(token));
                 var unprotectedToken = Encoding.UTF8.GetString(unprotectedData);
+                Logger.LogInformation("CheckTokenSignInAsync(2): token='{unprotectedToken}'", unprotectedToken);
+                var ms = new MemoryStream(unprotectedData);
+                using (var reader = ms.CreateReader())
+                {
+                    var creationTime = reader.ReadDateTimeOffset();
+                    Logger.LogInformation("CheckTokenSignInAsync(3): creationTime='{creationTime}'", creationTime);
+
+                    var expirationTime = creationTime + TimeSpan.FromDays(1);
+                    Logger.LogInformation("CheckTokenSignInAsync(4): expirationTime='{expirationTime}'", expirationTime);
+
+                    var uid = reader.ReadString();
+                    Logger.LogInformation("CheckTokenSignInAsync(5): userId='{uid}'", uid);
+
+                    var purp = reader.ReadString();
+                    Logger.LogInformation("CheckTokenSignInAsync(6): purpose='{purp}'", purp);
+
+                    var stamp = reader.ReadString();
+                    if (reader.PeekChar() != -1)
+                    {
+                        Logger.LogInformation("CheckTokenSignInAsync(7): error='Unexpected end of stream - security stamp'");
+                    }
+                    else
+                    {
+                        Logger.LogInformation("CheckTokenSignInAsync(7): securityStamp='{stamp}'", stamp);
+                    }
+                }
                 Logger.LogInformation("CheckTokenSignInAsync: token='{token}', user='{userId}', unprotectedToken='{unprotectedToken}'",
-                    token, await UserManager.GetUserIdAsync(user), unprotectedToken);
+                    token, userId, unprotectedToken);
             }
             catch (Exception ex)
             {
-                Logger.LogError(ex, "CheckTokenSignInAsync: token='{token}', user='{userId}', error='{Message}'", token, await UserManager.GetUserIdAsync(user), ex.Message);
+                Logger.LogError(ex, "CheckTokenSignInAsync: token='{token}', user='{userId}', error='{Message}'", token, userId, ex.Message);
             }
 
             if (await UserManager.VerifyUserTokenAsync(user, Options.Tokens.PasswordResetTokenProvider,
@@ -91,6 +118,24 @@ namespace Bit.Portal.Utilities
             Logger.LogWarning(2, "User {userId} failed to provide the correct enterprise portal token.",
                 await UserManager.GetUserIdAsync(user));
             return SignInResult.Failed;
+        }
+    }
+
+    /// <summary>
+    /// Utility extensions to streams
+    /// </summary>
+    internal static class StreamExtensions
+    {
+        internal static readonly Encoding DefaultEncoding = new UTF8Encoding(false, true);
+
+        public static BinaryReader CreateReader(this Stream stream)
+        {
+            return new BinaryReader(stream, DefaultEncoding, true);
+        }
+
+        public static DateTimeOffset ReadDateTimeOffset(this BinaryReader reader)
+        {
+            return new DateTimeOffset(reader.ReadInt64(), TimeSpan.Zero);
         }
     }
 }
